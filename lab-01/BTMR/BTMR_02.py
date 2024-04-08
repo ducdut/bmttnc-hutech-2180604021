57a82314bdded9ee403c6aab7248d77fd11eca2f
nguyenvanduc2010 <iloveyou2010@gmail.com>
2024-04-08 11:17:14 +0700

    [add] lab-lab02 vigenere

--- lab-01/BTMR/BTMR_02.py
+++ lab-01/BTMR/BTMR_02.py
@@ -4,21 +4,22 @@ def tinh_tong_so(string):
     # Tìm tất cả các số trong chuỗi sử dụng biểu thức chính quy
     numbers = re.findall(r'-?\d+', string)
 
-    # Khởi tạo tổng các số dương và âm
+    # Khởi tạo tổng các số dương và tổng các số âm
     tong_duong = 0
     tong_am = 0
 
-    # Tính tổng các số dương và âm
+    # Tính tổng các số dương và tổng các số âm
     for num in numbers:
         num = int(num)
         if num > 0:
             tong_duong += num
-        elif num < 0:
+        else:
             tong_am += num
 
     return tong_duong, tong_am
 
 chuoi = "-100#^sdfkj8902w3ir021@swf-20"
 tong_duong, tong_am = tinh_tong_so(chuoi)
-print("Giá trị dương:", tong_duong)
-print("Giá trị âm:", tong_am)
+
+print("Tổng các số dương trong chuỗi:", tong_duong)
+print("Tổng các số âm trong chuỗi:", tong_am)
